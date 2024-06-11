be60c1bb600ef172c2cd61dead1c3461aec8349e
FASami <foysal.developer@gmail.com>
2024-06-11 18:55:01 +0600

    chore: added prettier formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
 const HomePage = () => {
-  return <div className="text-center">HomePage</div>;
-};
+  return <div className="text-center">HomePage</div>
+}
 
-export default HomePage;
+export default HomePage
