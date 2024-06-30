fafdc4b37ba0f7cb26d695cf136a35d1229bdd7e
FASami <foysal.developer@gmail.com>
2024-06-30 16:12:52 +0600

    refactor: header navigation menu

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,8 +3,17 @@ import { Menu } from 'antd'
 import clsx from 'clsx'
 import { caveat } from '../fonts'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const Header = () => {
+  const navigation = [
+    { id: 1, url: '/artists', title: 'Artists' },
+    { id: 2, url: '/arts', title: 'Arts' },
+    { id: 3, url: '/contact', title: 'Contact' },
+    { id: 4, url: '/about', title: 'About' }
+  ]
+  const path = usePathname()
+
   return (
     <header className="h-20  w-screen z-50 bg-white relative">
       <div className="max-w-screen-xl px-3 mx-auto flex items-center justify-between h-full">
@@ -22,19 +31,22 @@ const Header = () => {
           </Link>
           <div className="h-1 bg-amber-500 w-32 mt-auto rounded-full" />
         </div>
-        <div className="flex items-center h-full gap-6 text-base font-semibold text-gray-700 tracking-wider relative px-4">
-          <Link className="cursor-pointer" href="/artists">
-            Artists
-          </Link>
-          <Link className="cursor-pointer" href="/arts">
-            Arts
-          </Link>
-          <Link className="cursor-pointer" href="/about">
-            About us
-          </Link>
-          <Link className="cursor-pointer" href="/contact">
-            Contact
-          </Link>
+        <div className="flex items-center gap-6 text-base font-semibold text-gray-700 tracking-wider relative px-4 h-full">
+          {navigation.map(({ id, title, url }) => {
+            return (
+              <Link
+                key={id}
+                className={clsx(
+                  'cursor-pointer  relative z-10 h-full flex items-center',
+                  path === url && 'border-b-4 border-b-amber-400 px-2'
+                )}
+                href={url}
+              >
+                <span>{title}</span>
+              </Link>
+            )
+          })}
+
           <div className="h-1 bg-gray-100 w-full mt-auto rounded-full absolute bottom-0 left-0 right-0" />
         </div>
         <div className="w-fit bg-amber-300 text-gray-900 font-semibold px-6 py-2 rounded-sm">
