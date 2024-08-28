4ecca5a8fdd960214628c6d239a64f28a44fca2f
FASami <foysal.developer@gmail.com>
2024-08-28 11:15:27 +0600

    feat: added recaptcha

---
+++ src/hooks/useRecaptcha.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import { useGoogleReCaptcha } from 'react-google-recaptcha-v3'
+
+const useReCaptcha = () => {
+  const { executeRecaptcha } = useGoogleReCaptcha()
+
+  const verifyReCaptcha = async (action: string) => {
+    if (!executeRecaptcha) {
+      return false
+    }
+    try {
+      const { data } = await axios.post('/api/auth/re-captcha', {
+        gRecaptchaToken: await executeRecaptcha(action)
+      })
+      console.log({ data })
+
+      return data.success
+    } catch (error) {
+      console.error(error)
+      return false
+    }
+  }
+
+  return { verifyReCaptcha }
+}
+export { useReCaptcha }
